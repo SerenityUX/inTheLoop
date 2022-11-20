07cb867730737aa5ec8f79ebb2b3675c1ff8f21b
Serenidad <95105859+SerenityUX@users.noreply.github.com>
2022-11-20 12:15:16 -0500

    current

--- components/dayDisplayComponent.js
+++ components/dayDisplayComponent.js
@@ -3,15 +3,46 @@ import {
     Text,
     TouchableOpacity,
     View,
+    Image,
   } from "react-native";
   import { events } from '../events.json';
+
+  const styles = StyleSheet.create({
+    image: {
+      width: 150,
+      height: 150,
+    }
+  });
 
 export default function dayDisplay( { date } ) {
 
     return(
         <View>
-            <TouchableOpacity onClick={() => console.log('ello')}><Text>{date[2][0]}/{date[1]}/{date[3]}</Text></TouchableOpacity>
-            <Text>{events[1].date[1]}/{events[1].date[2]}/{events[1].date[0]}</Text>
+            <Text>{date[2][0]}/{date[1]}/{date[3]}</Text>
+
+            {events.map((selectedEvent,i) =>
+
+            <View>
+                {[selectedEvent.date[1] + "", selectedEvent.date[2] + "", selectedEvent.date[0]] + "" == [date[2][0] + "",date[1] + "",date[3] + ""] ? (
+                    <View>
+                        <Text>{selectedEvent.name}</Text>
+                        <Text>{selectedEvent.details.description}</Text>
+                        {selectedEvent.details.images.map((imageUri, imageIndex) => 
+                        <View>
+                        <Image
+                        style={styles.image}
+                        source={{uri: imageUri}}
+                        />   
+                        </View>
+ 
+                        )}
+                    </View>
+                    ) : 
+                    (
+                        null
+                )}
+            </View>
+            )}
         </View>
         )
 }
