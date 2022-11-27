384c3e932d38d55b2492827a9277fe9487db0e00
Serenidad <95105859+SerenityUX@users.noreply.github.com>
2022-11-27 00:36:40 -0500

    current version

--- api.js
+++ api.js
@@ -3,16 +3,4 @@ import PocketBase from 'pocketbase';
 export const pb = new PocketBase('https://intheloop.fly.dev');
 
 // fetch a paginated records list
-export const resultList = await pb.collection('events').getList(1, 50, {
-    filter: 'created >= "2022-01-01 00:00:00" && someFiled1 != someField2',
-});
-
-// you can also fetch all records at once via getFullList
-export const records = await pb.collection('events').getFullList(200 /* batch size */, {
-    sort: '-created',
-});
-
-// or fetch only the first record that matches the specified filter
-export const record = await pb.collection('events').getFirstListItem('someField="test"', {
-    expand: 'relField1,relField2.subRelField',
-});
+export const resultList = await fetch("");
--- components/dayDisplayComponent.js
+++ components/dayDisplayComponent.js
@@ -3,7 +3,7 @@ import { events } from "../events.json";
 import Carousel from "react-native-reanimated-carousel";
 import { Dimensions } from "react-native";
 import EventPreview from "./eventPreview"
-
+import {useState} from "react"
 const styles = StyleSheet.create({
   image: {
     width: 150,
@@ -11,15 +11,26 @@ const styles = StyleSheet.create({
   },
 });
 
+
 export default function dayDisplay({ date, setModalVisible, setModalData }) {
   const windowWidth = Dimensions.get("window").width;
+  const noEvents = events.filter((selectedEvent, i) => (
+        [
+          selectedEvent.date[1] + "",
+          selectedEvent.date[2] + "",
+          selectedEvent.date[0],
+        ] +
+          "" ==
+        [date[2][0] + "", date[1] + "", date[3] + ""]
+    )).length != 0
 
   return (
     <View>
       <Text>
         {date[2][0]}/{date[1]}/{date[3]}
       </Text>
-
+      {!noEvents ? (<Text>No events</Text>) : (null)}
+      
       {events.map((selectedEvent, i) => (
         <View key={i}>
           {[
@@ -29,10 +40,11 @@ export default function dayDisplay({ date, setModalVisible, setModalData }) {
           ] +
             "" ==
           [date[2][0] + "", date[1] + "", date[3] + ""] ? (
-            <TouchableOpacity onPress={() => {
+            <TouchableOpacity 
+              
+              onPress={() => {
               setModalData(selectedEvent)
               setModalVisible(true)
-
             }}>
             <EventPreview selectedEvent={selectedEvent} windowWidth={windowWidth}/>
             </TouchableOpacity>
