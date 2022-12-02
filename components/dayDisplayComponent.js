f08f036dfe8571abe6f5ca3cfebfaa6c493acaaf
Serenidad <95105859+SerenityUX@users.noreply.github.com>
2022-12-02 16:08:45 -0500

    current

--- components/dayDisplayComponent.js
+++ components/dayDisplayComponent.js
@@ -4,6 +4,7 @@ import {
   TouchableOpacity,
   TouchableHighlight,
   View,
+  Pressable,
   Image,
 } from "react-native";
 import Carousel from "react-native-reanimated-carousel";
@@ -19,7 +20,9 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function dayDisplay({ date, setModalVisible, setModalData }) {
+export default function dayDisplay({ date, setModalVisible, setModalData, setAbsentVisible }) {
+  const dayofweek = require("dayofweek");
+
   const [events, setEvents] = useState([
     {
       date: [0, 0, 0],
@@ -62,10 +65,15 @@ export default function dayDisplay({ date, setModalVisible, setModalData }) {
 
   return (
     <View>
+      <View style={{flexDirection: "row", justifyContent: "space-between", marginLeft: 16, marginRight: 16, marginTop: 16}}>
       <Text>
-        {date[2][0]}/{date[1]}/{date[3]}
+        {dayofweek(date[3], date[2][0], date[1], 'long')}
       </Text>
-      {!noEvents ? (
+    <TouchableOpacity style={styles.button} onPress={() => setAbsentVisible(true)}>
+      <Text style={{color: "blue"}}>Plan Absence {date[2][0]}/{date[1]}/{date[3]}</Text>
+    </TouchableOpacity>  
+    </View>    
+    {!noEvents ? (
         <Text onPress={() => console.log(events)}>No events</Text>
       ) : null}
 
---
+++ screens/planAbsence.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  TextInput,
+  Image,
+  View,
+  Text,
+  SafeAreaView,
+  Button,
+  TouchableOpacity,
+  Dimensions,
+  Touchable,
+} from "react-native";
+import { StackActions } from "@react-navigation/native";
+import Carousel from "react-native-reanimated-carousel";
+import * as ImagePicker from "expo-image-picker";
+
+export default function AbsenceScreen({ navigation, onRequestClose, event }) {
+
+  return (
+    <View>
+        <Text>Absence Scheduler</Text>
+        
+    </View>
+  );
+}
